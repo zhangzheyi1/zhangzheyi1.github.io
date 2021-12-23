c08520573212c88ffbc024f90a59fc5bca8882f9
zhangzheyi <v_zeyizhang@tencent.com>
2021-12-23 15:05:01 +0800

    Site updated: 2021-12-23 15:05:00

--- demo/排序demo/练习.js
+++ demo/排序demo/练习.js
@@ -1,17 +1,5 @@
-function quickSort(list) {
-  const n = list.length;
-  if (n <= 1) {
-    return list;
+Function.prototype.myCall = function(ctx, ...args) {
+  if (!ctx) {
+    
   }
-  let pivot = list.splice(n/2, 1)[0];
-  let leftArr = [];
-  let rightArr = [];
-  for (let i = 0; i < n; i++) {
-    if (list[i] < pivot) {
-      leftArr.push(list[i]);
-    } else {
-      rightArr.push(list[i]);
-    }
-  }
-  return quickSort(leftArr).concat([pivot], quickSort(rightArr));
 }
