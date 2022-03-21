991b595e3fc8508764c4e2bfd120a782e25f9f35
zhangzheyi1 <v_zeyizhang@tencent.com>
2022-03-21 16:03:56 +0800

    Site updated: 2022-03-21 16:03:55

--- demo/只出现一次的字母.js
+++ demo/只出现一次的字母.js
@@ -1,27 +1,16 @@
-// 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+var labels={
+  cpu: "cpu-total",
+  ident: "10-255-0-34",
+  __name__: "cpu_usage_guest_nice"
+}
 
-/**
- * @param {string} s
- * @return {character}
- */
- var firstUniqChar = function(s) {
-  // 1、哈希
-  // let map = {}
-  // for (let i  = 0; i< s.length; i++) {
-  //     if (map[s[i]]) {
-  //         map[s[i]]++
-  //     } else {
-  //         map[s[i]] = 1
-  //     }
-  // }
-  // for (let i in map) {
-  //     if(map[i] === 1) return i
-  // }
-  // return ' '
+for (const key in object) {
+  console.log('key', key);
+}
 
-  // 2、indexOf和lastIndexOf
-  for (let i = 0; i< s.length; i++) {
-      if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return s[i]
+function a(value) {
+  for (const key in value) {
+    console.log('key', key);
   }
-  return ' '
-};
+}
+a(labels);
